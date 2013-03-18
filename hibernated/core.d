3c78a3caa079ef385ef8e80f86b5bc28601154db
Vadim Lopatin <buggins@fromru.com>
2013-03-18 15:00:24 +0400

    HibernateD: HQL query parser, support simple FROM Entity [[as] alias]; detect query parameters

--- hibernated/core.d
+++ hibernated/core.d
@@ -1,6 +1,7 @@
 module hibernated.core;
 
 import std.ascii;
+import std.algorithm;
 import std.exception;
 import std.string;
 
@@ -17,12 +18,130 @@ class SyntaxError : HibernatedException {
 	this(string msg, string f = __FILE__, size_t l = __LINE__) { super(msg, f, l); }
 }
 
-class Query {
+struct FromClauseItem {
+	string entityName;
+	EntityInfo entity;
+	string entityAlias;
+}
+
+class QueryParser {
+	string query;
 	EntityMetaData metadata;
 	Token[] tokens;
+	FromClauseItem[] fromClause;
+	string[] parameterNames;
 	this(EntityMetaData metadata, string query) {
 		this.metadata = metadata;
+		this.query = query;
 		tokens = tokenize(query.dup);
+		parse();
+	}
+
+	void parse() {
+		int len = cast(int)tokens.length;
+		processParameterNames(0, len); // replace pairs {: Ident} with single Parameter token
+		int fromPos = findKeyword(KeywordType.FROM);
+		int selectPos = findKeyword(KeywordType.SELECT);
+		int wherePos = findKeyword(KeywordType.WHERE);
+		int orderPos = findKeyword(KeywordType.ORDER);
+		enforceEx!SyntaxError(fromPos >= 0, "No FROM clause in query " ~ query);
+		enforceEx!SyntaxError(selectPos <= 0, "SELECT clause should be first - invalid query " ~ query);
+		enforceEx!SyntaxError(wherePos == -1 || wherePos > fromPos, "Invalid WHERE position in query " ~ query);
+		enforceEx!SyntaxError(orderPos == -1 || (orderPos < tokens.length - 2 && tokens[orderPos + 1].keyword == KeywordType.BY), "Invalid ORDER BY in query " ~ query);
+		enforceEx!SyntaxError(orderPos == -1 || orderPos > fromPos, "Invalid ORDER BY position in query " ~ query);
+		int fromEnd = len;
+		if (orderPos >= 0)
+			fromEnd = orderPos;
+		if (wherePos >= 0)
+			fromEnd = wherePos;
+		int whereEnd = wherePos < 0 ? -1 : (orderPos >= 0 ? orderPos : len);
+		int orderEnd = orderPos < 0 ? -1 : len;
+		parseFromClause(fromPos + 1, fromEnd);
+		if (selectPos == 0 && selectPos < wherePos - 1)
+			parseSelectClause(selectPos + 1, wherePos);
+		if (wherePos >= 0 && whereEnd > wherePos)
+			parseWhereClause(wherePos + 1, whereEnd);
+		if (orderPos >= 0 && orderEnd > orderPos)
+			parseOrderClause(orderPos + 2, orderEnd);
+	}
+
+	private void updateEntity(EntityInfo entity, string name) {
+		foreach(t; tokens) {
+			if (t.type == TokenType.Ident && t.text == name) {
+				t.entity = entity;
+				t.type = TokenType.Entity;
+			}
+		}
+	}
+
+	private void updateAlias(EntityInfo entity, string name) {
+		foreach(t; tokens) {
+			if (t.type == TokenType.Ident && t.text == name) {
+				t.entity = entity;
+				t.type = TokenType.Alias;
+			}
+		}
+	}
+	
+	void parseFromClause(int start, int end) {
+		enforceEx!SyntaxError(start < end, "Invalid FROM clause in query " ~ query);
+		// minimal support:
+		//    Entity
+		//    Entity alias
+		//    Entity AS alias
+		enforceEx!SyntaxError(tokens[start].type == TokenType.Ident, "Entity name identifier expected in FROM clause in query " ~ query);
+		string entityName = cast(string)tokens[start].text;
+		EntityInfo ei = metadata.findEntity(entityName);
+		updateEntity(ei, entityName);
+		string aliasName = null;
+		int p = start + 1;
+		if (p < end && tokens[p].type == TokenType.Keyword && tokens[p].keyword == KeywordType.AS)
+			p++;
+		if (p < end) {
+			enforceEx!SyntaxError(tokens[p].type == TokenType.Ident, "Alias name identifier expected in FROM clause in query " ~ query);
+			aliasName = cast(string)tokens[p].text;
+			p++;
+		}
+		enforceEx!SyntaxError(p == end, "Extra items in FROM clause (only simple FROM Entity [[as] alias] supported so far) - in query " ~ query);
+		if (aliasName != null)
+			updateAlias(ei, aliasName);
+		fromClause = new FromClauseItem[1];
+		fromClause[0].entityName = entityName;
+		fromClause[0].entity = ei;
+		fromClause[0].entityAlias = aliasName;
+	}
+
+	// in pairs {: Ident} replace type of ident with Parameter 
+	void processParameterNames(int start, int end) {
+		for (int i = end - 2; i >= start; i--) {
+			if (tokens[i].type == TokenType.Colon) {
+				enforceEx!SyntaxError(tokens[i + 1].type == TokenType.Ident, "Parameter name expected after : in WHERE clause - in query " ~ query);
+				parameterNames ~= cast(string)tokens[i + 1].text;
+				tokens[i + 1].type = TokenType.Parameter;
+				remove(tokens, i);
+			}
+		}
+	}
+
+	void parseSelectClause(int start, int end) {
+		enforceEx!SyntaxError(start < end, "Invalid SELECT clause in query " ~ query);
+	}
+
+	void parseWhereClause(int start, int end) {
+		enforceEx!SyntaxError(start < end, "Invalid WHERE clause in query " ~ query);
+	}
+	
+	void parseOrderClause(int start, int end) {
+		enforceEx!SyntaxError(start < end, "Invalid ORDER BY clause in query " ~ query);
+	}
+	
+	/// returns position of keyword in tokens array, -1 if not found
+	int findKeyword(KeywordType k, int startFrom = 0) {
+		for (int i = startFrom; i < tokens.length; i++) {
+			if (tokens[i].type == TokenType.Keyword && tokens[i].keyword == k)
+				return i;
+		}
+		return -1;
 	}
 
 }
@@ -137,6 +256,11 @@ enum TokenType {
 	Dot,          // .
 	OpenBracket,  // (
 	CloseBracket, // )
+	Colon,        // :
+	Entity,       // entity name
+	Field,        // field name of some entity
+	Alias,        // alias name of some entity
+	Parameter,    // ident after :
 }
 
 class Token {
@@ -145,6 +269,8 @@ class Token {
 	OperatorType operator = OperatorType.NONE;
 	char[] text;
 	char[] spaceAfter;
+	EntityInfo entity;
+	PropertyInfo field;
 	this(TokenType type, string text) {
 		this.type = type;
 		this.text = text.dup;
@@ -302,6 +428,8 @@ Token[] tokenize(char[] s) {
 			res ~= new Token(TokenType.OpenBracket, "(");
 		} else if (ch == ')') {
 			res ~= new Token(TokenType.CloseBracket, ")");
+		} else if (ch == ':') {
+			res ~= new Token(TokenType.Colon, ":");
 		} else {
 			enforceEx!SyntaxError(false, "Invalid token near " ~ cast(string)s[startpos .. $]);
 		}
@@ -311,12 +439,24 @@ Token[] tokenize(char[] s) {
 
 unittest {
 	Token[] tokens;
-	tokens = tokenize("SELECT a From User a where a.flags = 1 AND a.name='john' ORDER BY a.idx ASC");
+	tokens = tokenize("SELECT a From User a where a.flags = 12 AND a.name='john' ORDER BY a.idx ASC");
 	assert(tokens.length == 23);
 	assert(tokens[0].type == TokenType.Keyword);
 	assert(tokens[2].type == TokenType.Keyword);
 	assert(tokens[5].type == TokenType.Keyword);
 	assert(tokens[5].text == "where");
+	assert(tokens[10].type == TokenType.Number);
+	assert(tokens[10].text == "12");
 	assert(tokens[16].type == TokenType.String);
 	assert(tokens[16].text == "john");
+	assert(tokens[22].type == TokenType.Keyword);
+	assert(tokens[22].text == "ASC");
+
+	EntityMetaData schema = new SchemaInfoImpl!(User, Customer);
+	QueryParser parser = new QueryParser(schema, "SELECT a FROM User AS a WHERE id = :Id");
+	assert(parser.parameterNames.length == 1);
+	assert(parser.parameterNames[0] == "Id");
+	assert(parser.fromClause.length == 1);
+	assert(parser.fromClause[0].entity.name == "User");
+	assert(parser.fromClause[0].entityAlias == "a");
 }
