75b949982851acddc0fd0d5108f57c826fa515c8
Vadim Lopatin <buggins@fromru.com>
2013-06-19 18:34:37 +0400

    Fix test example

--- hdtest/htestmain.d
+++ hdtest/htestmain.d
@@ -2,6 +2,7 @@ module htestmain;
 
 import std.stdio;
 import std.string;
+import std.conv;
 import ddbc.drivers.sqliteddbc;
 import hibernated.core;
 import hibernated.dialects.sqlitedialect;
@@ -34,11 +35,21 @@ void testHibernate() {
     writeln("Creating schema from module list");
     EntityMetaData schema2 = new SchemaInfoImpl!(htestmain);
 
+
     writeln("Creating session factory");
     // create session factory
     Dialect dialect = new SQLiteDialect();
     SessionFactory factory = new SessionFactoryImpl(schema, dialect, ds);
     scope(exit) factory.close();
+
+    writeln("Creating DB schema");
+    DBInfo db = factory.getDBMetaData();
+    {
+        Connection conn = ds.getConnection();
+        scope(exit) conn.close();
+        db.updateDBSchema(conn, true, true);
+    }
+
 
     // create session
     Session sess = factory.openSession();
@@ -46,10 +57,13 @@ void testHibernate() {
 
     // use session to access DB
 
+    writeln("Querying empty DB");
     Query q = sess.createQuery("FROM User ORDER BY name");
     User[] list = q.list!User();
+    writeln("Result size is " ~ to!string(list.length));
 
     // create sample data
+    writeln("Creating sample schema");
     Role r10 = new Role();
     r10.name = "role10";
     Role r11 = new Role();
@@ -57,34 +71,36 @@ void testHibernate() {
     User u10 = new User();
     u10.name = "Alex";
     u10.roles = [r10, r11];
+    writeln("Saving r10");
     sess.save(r10);
+    writeln("Saving r11");
     sess.save(r11);
+    writeln("Saving u10");
     sess.save(u10);
 
+    writeln("Loading User");
     // load and check data
     User u11 = sess.createQuery("FROM User WHERE name=:Name").setParameter("Name", "Alex").uniqueResult!User();
+    writeln("Checking User");
     assert(u11.roles.length == 2);
     assert(u11.roles[0].name == "role10" || u11.roles.get()[0].name == "role11");
     assert(u11.roles[1].name == "role10" || u11.roles.get()[1].name == "role11");
     assert(u11.roles[0].users.length == 1);
     assert(u11.roles[0].users[0] == u10);
 
+    writeln("Removing User");
     // remove reference
-    //u11.roles.get().remove(0);
-    //sess.update(u11);
+    std.algorithm.remove(u11.roles.get(), 0);
+    sess.update(u11);
 
     // remove entity
-    //sess.remove(u11);
+    sess.remove(u11);
 }
 
 void main()
 {
-    writeln("Before test hibernate");
-    writeln("import for User: " ~ generateImportFor!User);
-    writeln("fqname for htestmain: " ~ fullyQualifiedName!htestmain);
-    writeln("fqname for std.string: " ~ fullyQualifiedName!(std.string));
     testHibernate();
-    writeln("Hello D-World!");
+    writeln("Press any key");
     readln();
 
 }
