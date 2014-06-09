6fe30ac07c98c1fdd95070eec00938dcb2f588ba
Dmitriy Kulik <lnkvisitor@altlinux.org>
2014-06-10 01:32:28 +0300

    Compilation fix
    Warning: statement is not reachable in metadata.d:1120

--- source/hibernated/metadata.d
+++ source/hibernated/metadata.d
@@ -1117,7 +1117,6 @@ bool isMainMemberForProperty(T : Object, string m)() {
             static assert (!thisMemberHasAnnotation || !hasAnnotadedGetter, "Both getter and corresponding field have annotations. Annotate only one of them.");
             return !hasAnnotadedGetter && (thisMemberHasAnnotation || !hasGetter);
         }
-        return false;
     } else {
         // member is not public
         return false;
